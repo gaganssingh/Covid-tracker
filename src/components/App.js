c18f18e01e3119dcbe41df7ab4121c0e08ae1b9a
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-12 13:12:48 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const App = () => {
+   const [data, setData] = useState({});
+   const [country, setCountry] = useState("");
+
+   return <h1>App</h1>;
+};
+
+export default App;
